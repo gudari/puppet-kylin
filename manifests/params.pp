cc97de5279383549fd80f2a75c1c2b5ed3b7403e
Asier Artola <axier1986@gmail.com>
2017-05-12 15:52:49 +0200

    add default params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,17 +1,20 @@
 class kylin::params {
 
-  $version       = '1.6.0'
-  $hbase_version = '1.x'
+  $version        = '1.6.0'
+  $hbase_version  = '1.x'
 
-  $mirror_url    = 'http://apache.rediris.es'
-  $install_dir   = '/opt/kylin'
-  $download_dir  = '/var/tmp/kylin'
-  $log_dir       = '/opt/log'
-  $pid_dir       = '/var/run/kylin'
+  $mirror_url     = 'http://apache.rediris.es'
+  $install_dir    = '/opt/kylin'
+  $download_dir   = '/var/tmp/kylin'
+  $log_dir        = '/opt/log'
+  $pid_dir        = '/var/run/kylin'
 
-  $kylin_user    = 'kylin'
-  $kylin_uid     = undef
-  $kylin_group   = 'kylin'
-  $kylin_gid     = undef
+  $kylin_user     = 'kylin'
+  $kylin_uid      = undef
+  $kylin_group    = 'kylin'
+  $kylin_gid      = undef
+
+  $package_name   = undef
+  $package_ensure = installed
 
 }
